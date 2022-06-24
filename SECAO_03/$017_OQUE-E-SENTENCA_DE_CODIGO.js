67ceab69f8c9c34e0373ba108c97ac1e01690498
Fabiano <fasnaweb2020@gmail.com>
2022-06-23 21:48:37 -0300

    O que são sentenças de código

---
+++ SECAO_03/$017_OQUE-E-SENTENCA_DE_CODIGO.js
@@ -0,0 +1,15 @@
+/**
+ *  Uma sentenca de codigo em javaScript e uma linha de codigo ou instrucao.Dado como exemplo a linha abaixo:
+ *  console.log("Insto e uma sentenca de codigo.")
+ * 
+ *  Uma sentenca de codigo pode conter ou nao no seu final um ponto e virgula (;) A sentenca de codigo abaixo tambem e valida
+ *  console.log("Insto e uma sentenca de codigo.");
+ * 
+ *  Entao uma sentenca de codigo tem por objetivo transmitir para o computador instrucoes,como faca isso ou aquilo.
+ *  Sentencas de codigo compoe um algoritmo,uma embaixo da outra,sendo as mesmas lidas uma de cada vez de cima para baixo.
+ *  
+ */
+  
+console.log("Vai ler essa primeira sentença de codigo")
+console.log("Vai imprimir a segunda sequencia de codigo soma = ", 2 + 2)
+console.log("Vai imprimir a terceira sequencia de codigo, o quadrado de 2 e = ",Math.pow(2,2))
