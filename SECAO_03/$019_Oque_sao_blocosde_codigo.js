32f92dda810eeec79011df69e699d4eba0ba0e1a
Fabiano <fasnaweb2020@gmail.com>
2022-06-23 22:45:30 -0300

    O que sao bloco de codigo

---
+++ SECAO_03/$019_Oque_sao_blocosde_codigo.js
@@ -0,0 +1,10 @@
+// Blocos de codigos sao representados por {}
+
+{// inicio do bloco
+  // Corpo do bloco de codigo,local onde voce ira agrupar varias sentencas de codigo
+}//fim do bloco
+
+{
+    console.log("Esta dentro de um bloco de codigo")
+}
+
