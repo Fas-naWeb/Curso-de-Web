55b2d180f0a031e0cdd86dadf034c5f12bc1dfd0
Fabiano <fasnaweb2020@gmail.com>
2022-06-23 22:17:30 -0300

    Entendendo como funciona os comentarios de código

---
+++ SECAO_03/$018_SOBRE_COMENTARIOS.js
@@ -0,0 +1,24 @@
+// A tres formas de se comentar um codigo,que sao comentarios de uma linha e de varias linhas.
+
+//Voce pode comentar um codigo de varias formas 
+ 
+
+ function verNomes(){ // Posso comentar aqui como inicio da minha funcao
+    return "Fabiano Alves Santos"
+ }// E aqui como fim da minha funcao,não atrapalha na execusao do codigo
+
+ /**
+  * De um modo geral quando voce usa // significa que voce esta fazendo um comentario de uma linha
+  * quando voce usa barra asteristico e asteristico barra significa que voce esta utilizando um co
+  * mentario de varias linhas. 
+  * 
+  */
+
+  /**
+   * Mais para que utilizar comentarios?
+   *  A resposta e simples,comentarios servem para descrever algo que voce julga importante guardar
+   * ou informar a alguem ou para num futuro proximo te lembrar de um detalhe que voce fez naquele
+   * repectivo trecho de codigo.E uma informacao que servira para voce mesmo se lembrar ou para outros
+   * desenvolvedores que for fazer uma manutencao no seu codigo ficar informado porque voce escreveu o mesmo
+   * daquela forma.
+   */
