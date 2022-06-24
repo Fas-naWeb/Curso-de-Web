1048febf22b9767d6dc969d8dbe780a17d45a94a
Fabiano <fasnaweb2020@gmail.com>
2022-06-23 21:55:52 -0300

    Algumas dicas sobre a utilização do vsCode

---
+++ DICAS_DO_VSCODE.js
@@ -0,0 +1,5 @@
+/**
+ * Se voce segurar as teclas shift+alt e seta para baixo ou pra cima voce gera uma copia do codigo
+ * Se voce segurar a tecla alt+seta para cima ou para baixo,voce sobe o codigo ou desce a linha de codigo
+ */
+bbbb
