04aa954fe739d520d95c9859a9dba750a9475b3a
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-06-25 20:53:26 -0300

    Entendendo o que sao estrutura de dados

---
+++ SECAO_04/$022_SobreEstruturaDeDados.js
@@ -0,0 +1,9 @@
+/**
+ * O que sao estrutura de dados?
+ *   Estrutura de dados visa organizar dados.
+ *   Um exemplo são as listas
+ *   Outra estrura de dados e a fila -> Primeiro que entre e o primeiro que sai
+ *   Outra estrutura e a pilha onde o ultimo a entrar e o primeiro a sair
+ *   Arvores tambem sao estrutura de dados 
+ *   Tabelas tambem sao estruturas que organizam dados
+ */
