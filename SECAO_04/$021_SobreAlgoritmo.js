06f1414d78f8aec0bda27eea9c8767296ed69b5c
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-06-25 19:53:53 -0300

    Entendendo o que e algoritmo

---
+++ SECAO_04/$021_SobreAlgoritmo.js
@@ -0,0 +1,40 @@
+/**
+ * Em uma visao geral algoritmo e usado a todo momento, tanto na nossa vida no dia a dia 
+ * como tambem em computacao.Podemos dizer que algoritmo nao e software,mais utilizamos 
+ * algoritmos para criar software. 
+ * Algoritmo e uma sequencia de passos,essa sequencia de passos pode ser para resolver 
+ * algum problema ou não.Em computacao algoritmos serve para resolver problemas na construcao
+ * de software.Na solucao de problemas computacional voce utiliza de recursos da lin-
+ * guagem de programacao para construir algoritmos,como sentenca de codigo,blocos de codigo
+ * estruturas de selecao e de repeticao,assim como tipos primitivos e variaveis que em uma 
+ * sequencia logica um embaixo do outra vai gerar um resultado.
+ *  
+ *   Um exemplo simples de algoritmo computacional e fazer a media do aluno. 
+ */
+
+    let nota1 =12
+    let nota2 = 3
+    let nota3 = 22
+
+    let soma = nota1 + nota2 + nota3
+    let media = soma / 3
+    console.log("Media do aluno e: ",media)
+
+    /**
+     * ALGORITMO RECEITA DE BOLO
+     * Passo 1 - Receber os ingredientes
+     *  2 xicaras de acucar
+     *  3 ovos
+     *  250g de margarina
+     *  3 xicaras de farinha de trigo
+     *  1 e 1/2 colher de fermento
+     *  1 xicara de leite
+     * 
+     * Passo 2 - Aqueca o forno a 180 graus 
+     *  
+     * Passo 3 - batas as claras em neve e reserve
+     * 
+     * Passo 4 - Em uma travessa,bata o acucar, a manteiga e as gemas 
+     * 
+     * Passo 5 .....................
+     */
