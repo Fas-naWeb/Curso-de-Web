b98572f651f727825d0e2997f2b7affa8a67aeff
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-06-26 00:21:22 -0300

    Sobre tipagem fraca em javScript

---
+++ SECAO_04/$025_SobreTipagemJavaScript.js
@@ -0,0 +1,26 @@
+/*
+    Resumindo,em javaScript não se faz declaracao de tipo como em outras linguagens.
+    o tipo da variavel e definido pelo seu conteudo.Em java por exemplo quando se declara 
+    uma variavel do tipo inteiro devevos identificar primeiro o tipo e associar a seu rotulo
+    e o conteudo tem que ser do tipo declarado,se nao gera um erro na compilacao do codigo
+    ja em javaScript o seu conteudo determina seu tipo.
+*/
+
+// Exemplos
+// Aqui a variavel qualquer assume um tipo string
+let qualquer = "frase Qualquer"
+console.log(typeof qualquer)
+
+// Aqui a variavel qualquer assume um tipo numero inteiro
+qualquer = 29
+console.log(typeof qualquer)
+
+// Aqui a variavel qualquer assume um tipo numero de ponto flutuante (real)
+qualquer = 3.4
+console.log(typeof qualquer)
+
+// Aqui a variavel qualquer assume um tipo boolean
+qualquer = true
+console.log(typeof qualquer)
+
+
