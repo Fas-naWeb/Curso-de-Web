6d416d115f06f2c82f876439ada71b5e1983cec1
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-06-25 21:15:04 -0300

    Entendendo como um codigo javaScript e organizado

---
+++ SECAO_04/$023_ComoEorganizadoCodigoJavaScript.js
@@ -0,0 +1,25 @@
+/**
+ * O codigo javaScript e organizado em sentenca de codigo e em bloco de codigo
+ * voce tem bloco de codigo associado a funcoes,a estrutura de repeticao etc...
+ */
+
+//Aqui e uma senteca de codigo
+console.log("Esse trecho de codigo e uma sentenca de codigo.")
+
+{//Aqui e um inicio de um bloco de codigo
+  console.log("Blocos de codigo em javaScript sao representados po chaves {}")
+}//fim do bloco de codigo
+
+{
+  {
+    {
+        {
+            console.log("Voce pode criar blocos de codigo um dentro do outro.")
+        }
+    }
+  }
+}
+
+function soma(){// inicio
+    return console.log("Aqui tem um bloco de codigo associado a uma funcao.")
+}//fim 
