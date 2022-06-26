553fa3f314336b26faef71127649d0283e1b63fc
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-06-25 22:01:21 -0300

    Sobre variaveis

---
+++ SECAO_04/$024_SobreVariaveis.js
@@ -0,0 +1,19 @@
+// Var e o que identifica a declaracao da variavel
+// a E o rotulo que voce da para variavel
+// = E o sinal de atribuicao
+// 3 E o valor guardado na variavel
+var a = 3
+
+let b = 5
+console.log(a,b)
+// Variaveis podem ter seus valores trocados substituidos
+a = 30
+b = 500
+console.log(a,b)
+
+// Voce pode criar variaveis com var,let e const
+
+// As constantes geralmente nao podem ter seus valores subistituidos
+// Dica se sua variavel nao for ter o valor subistituido declarea como const
+const numero = 38
+//numero = 87 // vai gerar um erro
