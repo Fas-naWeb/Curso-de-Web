f2bf4c89def47ff6f8fa6c2072978feee81f656f
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-06-28 22:18:22 -0300

    Entendendo o que e objeto e como criar - lo

---
+++ SECAO_04/$032_SobreObjetos.js
@@ -0,0 +1,24 @@
+// Em javaScript um objeto e criado dessa forma,os pares de chaves definem que e um objeto.
+// Nesse caso foi criado um objeto vazio,voce pode i adicionando propriedades a ele dinamicamente
+
+const objeto = {}
+objeto.mome = "Fabiano Alves Santos"
+objeto.email = "fasnaweb2020@gmail.com"
+objeto.telefone = "Se alguem me ligar e porque realmente visualizou meu codigo 998985204"
+objeto.altura = 1.76
+
+// Note que um objeto e constituido por chave valor...
+console.log(objeto)
+
+// Outra forma de criar objetos em javaScript
+const obj2 = {
+    mome: "Fabiano Alves Santos",
+    email: "fasnaweb2020@gmail.com",
+    telefone: "Se alguem me ligar e porque realmente visualizou meu codigo 998985204",
+    altura: 1.76,
+
+    outros: {
+       endereco:"Rua M ....."
+    }
+} 
+console.log(objeto)
