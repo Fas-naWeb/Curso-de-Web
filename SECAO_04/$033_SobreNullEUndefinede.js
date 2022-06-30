76e3249a48d6d9a2de124fcf4fab990372b4c198
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-06-30 00:46:29 -0300

    Entendendo o por que de null e undefiined

---
+++ SECAO_04/$033_SobreNullEUndefinede.js
@@ -0,0 +1,17 @@
+/**
+ * Diferenca entre undefined e not defined
+ */
+
+// Aqui a variavel foi declarada porem nao recebeu nenhum valor,o resultado aqui vai ser undefined
+let nome;
+console.log(nome)
+
+// Ja se eu querer imprimir uma variavel que nao foi declarada,isso ira retornar not defined
+//console.log(nome2);
+
+// Null nao aponta paralugar nenhum
+let dia = null
+
+const produto = {}
+console.log(produto.preco);
+//Pense nessa situacao e me responda ....null ou undefined
