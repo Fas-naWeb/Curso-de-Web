cff0e7c80a2a918113ec67f21171cfbec87e6cab
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-05 00:00:21 -0300

    Sobre par chave valor

---
+++ SECAO_04/$042_SobreParChaveValor.js
@@ -0,0 +1,18 @@
+// Par chave valor conceito lexeco
+const saudacao = "Epa!!!"
+
+function sexc(){
+    const saudacao = "Falaa!!!"
+    return saudacao
+}
+
+// Objetos sao grupos aninhados de chave valor
+const Cliente = {
+    nome:"Fabiano",
+    idade:41,
+     endereco: {
+       Rua:"Maria Jose pereira ...."
+    }
+}
+console.log(sexc())
+console.log(Cliente)
