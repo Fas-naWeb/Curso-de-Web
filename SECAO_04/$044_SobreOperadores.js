01be20e4aad3a3830d80268a8c9b7c7e5163bb31
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-05 00:15:36 -0300

    Sobre os operadores comuns

---
+++ SECAO_04/$044_SobreOperadores.js
@@ -0,0 +1,22 @@
+const a = 2
+console.log(a)
+
+console.log()
+
+let b = 4
+console.log(b)
+
+b += 2
+console.log(b)
+
+b -= 2
+console.log(b)
+
+b *= 2
+console.log(b)
+
+b /= 2
+console.log(b)
+
+b %= 2
+console.log(b)
