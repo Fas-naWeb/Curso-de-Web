151a4e2b2ee39ce99a3980e7196b85b000c240a6
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-04 21:25:12 -0300

    Entendendo o conceito de hosting

---
+++ SECAO_04/$040_EntendendoOhosting.js
@@ -0,0 +1,10 @@
+// conceito de hosting e de icar ou seja de subir algo,nesse contexto temos a eleacao da variavel
+
+console.log(a)
+var a =7
+console.log(a)
+
+// Ocorre aqui que primeiro e impresso undefined e depois o valor 7
+// O na primeira situacao acontece o hosting,a variaval a e icada,porem sem valor gerando undefined,significa
+// que o  javaScript identificou que existe uma variavel a e chamou - a para cima sem valor.
+// No segundo console.log() imprimiu 7 pois a declaracao foi feita antes.
