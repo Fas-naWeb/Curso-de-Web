1b5a5b59078da0e02e0b01c37ba4e4549921cb3c
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-07 21:50:01 -0300

    Entendendo os operadores relacionais

---
+++ SECAO_04/$049_SobreOperadoresRelacionais.js
@@ -0,0 +1,26 @@
+console.log("SOBRE OS OPERADORES RELACIONAIS")
+
+console.log("1)  '1' == 1 R: ",'1' == 1)
+console.log("2)  '1' === 1 R: ",'1' === 1)
+console.log("3)  '3' != 3 R:",'3' != 3)
+console.log("4)  '3' !== 3 R:",'3' !== 3)
+console.log("5)  3 < 2 R: ", 3< 2)
+console.log("6)  3 > 2 R: ", 3 > 2)
+console.log("7)  3 <= 2 R: ",3 <= 2)
+console.log("8)  3 >= 2 R: ",3 >= 2)
+
+console.log()
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+
+console.log("const d1 = new Date(0)")
+console.log("const d2 = new Date(0)")
+
+console.log()
+
+console.log("9)  d1 == d2 R: ",d1 == d2)
+console.log("10)  d1 === d2 R: ",d1 === d2)
+console.log("11)  d1.getTime() === d2 R: ",d1.getTime() === d2)
+console.log("12)  undefined == null R: ",undefined == null)
+console.log("12)  undefined === null R: ",undefined === null)
