dfd64e3a3848045d04aeb638462615bd173a4223
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-07 21:21:38 -0300

    Aprendendo sobre operadores aritmeticos

---
+++ SECAO_04/$048_SobreOperadores.js
@@ -0,0 +1,14 @@
+const [a,b,c,d,e] = [1,2,3,4,5]
+
+const soma = a + b + c + d + e
+const subtracao = b - e
+const multiplicacao = d * d
+const divisao = d / b
+const resto = a % b
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(resto)
+
