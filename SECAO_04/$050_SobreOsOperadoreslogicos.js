56fc5fbc8e23499adab193a68d2eb16ea858b80e
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-10 13:46:06 -0300

    Entendendo a tabela verdade e os operadores logicos

---
+++ SECAO_04/$050_SobreOsOperadoreslogicos.js
@@ -0,0 +1,19 @@
+console.log("SOBRE A TABELA VERDADE")
+console.log()
+console.log("Sobre o operando &&")
+console.log("v && v = v")
+console.log("v && f = f")
+console.log("f && v = f")
+console.log("f && f = f")
+
+console.log("------------")
+
+console.log("v || v = v")
+console.log("v || f = v")
+console.log("f || v = f")
+console.log("f || f = f")
+
+console.log("------------")
+
+
+
