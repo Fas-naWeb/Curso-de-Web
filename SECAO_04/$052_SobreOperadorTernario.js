668b43eff1a70e3e50baa27068b679a16ba3d8d4
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-10 14:20:06 -0300

    Sobre como utilizar o operador ternario

---
+++ SECAO_04/$052_SobreOperadorTernario.js
@@ -0,0 +1,9 @@
+/**
+ * O operador ternario funciona em duas partes.Voce primeiro tem a condicao logica
+ * depois o resultado antes dos dois pontos para verdadeiro e depois dos dois pontos para felso
+ * Exemplo....
+ */
+let nota = 8
+const resultado = (nota >= 7)? "Aprovado":"Reprovado"
+console.log(resultado) 
+
