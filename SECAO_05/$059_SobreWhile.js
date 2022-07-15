72c00e7c8525e8d313079f2f650311e046033564
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-14 23:21:00 -0300

    Como utilizar a estrutura a estrutura while

---
+++ SECAO_05/$059_SobreWhile.js
@@ -0,0 +1,18 @@
+// While e uma estrutura de repeticao que pode ser utilizado com um contador
+// e tambem com uma situacao que gera falso,porem nao e muito usual porque gera um loop infinito
+// Exemplo:
+
+let opcao = "Fabiano"
+let contador = 0
+
+while (opcao === "Fabiano") {
+    console.log("Fabiano")
+}
+
+console.log()
+
+while (contador <= 20) {
+    console.log("Fabiano")
+    contator++
+}
+
