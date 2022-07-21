2b462b1158fea381c3cad85787791cf3e94926ec
Fabiano Alves Santos <fasnaweb2020@gmail.com>
2022-07-20 23:57:03 -0300

    Utilizando outros arrow function parte2

---
+++ SECAO_06/$070_SobreFuncaoArrowP2.js
@@ -0,0 +1,10 @@
+// This ele nao varia com funcoes arrow
+function Pessoa() {
+    this.idade = 0
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+
+}
+new Pessoa
